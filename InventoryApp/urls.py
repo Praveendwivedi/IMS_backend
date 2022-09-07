f7ddce899f6da9646db840d5fba859cffa6ad044
Praveendwivedi <praveen98ok@gmail.com>
2022-09-07 16:21:29 +0530

    modified url

--- InventoryApp/urls.py
+++ InventoryApp/urls.py
@@ -2,15 +2,15 @@
 from . import views
 from django.urls import  re_path
 
-from django.conf.urls.static import static
-from django.conf import settings
 
 urlpatterns=[
     re_path(r"^factory$",views.factoryApi),
     re_path(r"^factory/([0-9]+)$",views.factoryApi),
 
-    re_path(r"^product$",views.productApi),
-    re_path(r"^product/([0-9]+)$",views.productApi),
-
+    re_path(r"^product$",views.productClass.as_view()),
+    re_path(r"^product/([0-9]+)$",views.productClass.as_view()),
+    re_path(r"^products$",views.productClass.as_view()),
+    re_path(r"^products/detail$",views.productDetail.as_view()),
+    re_path(r"^products/detail/([0-9]+)$",views.productDetail.as_view()),
     re_path(r"^product/savefile",views.SaveFile)
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]
